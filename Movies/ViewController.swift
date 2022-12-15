85e61825a8704d2346caa9d63633d23485258091
Nayani Gaonkar <nayani.gaonkar@sjinnovation.com>
2022-12-16 05:22:53 +0530

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by SJI-GOA-79 on 16/12/22.
+//
+
+import UIKit
+import Amplify
+import AWSAPIPlugin
+import AWSDataStorePlugin
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "movies9693b8b9": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
